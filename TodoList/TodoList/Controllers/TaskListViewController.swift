31d3c2cef574a940c4f446da331b139d1ac4539b
JooYoo <jooyoo@outlook.com>
2022-08-15 14:38:15 +0200

    style: added task-list-view

---
+++ TodoList/TodoList/Controllers/TaskListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TodoListViewController.swift
+//  TodoList
+//
+//  Created by Yu on 15.08.22.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class TaskListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell
+    }
+}
+
