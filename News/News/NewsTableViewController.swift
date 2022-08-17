7e0c5089bcd76893f877b7b6afdc5568d46c662c
JooYoo <jooyoo@outlook.com>
2022-08-17 13:26:42 +0200

    chore: installed rxswift

--- News/News/NewsTableViewController.swift
+++ News/News/NewsTableViewController.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
     
@@ -16,5 +18,20 @@ class NewsTableViewController: UITableViewController {
         // UI
         /// set title to large
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // get API_KEY
+        let key = getKey()
+        print(key)
+    }
+    
+    func getKey() -> String{
+        // get apiKey from Bundle
+        let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+        // check if key is ok
+        guard let key = apiKey, !key.isEmpty else {
+            print("API key does not exist")
+            return ""
+        }
+        return key
     }
 }
