cc22c4e40324f4a2b22cb1bfb38e3eeed6e2d00b
JooYoo <jooyoo@outlook.com>
2022-08-17 10:17:41 +0200

    chore: managed api key

--- News/News/ViewController.swift
+++ News/News/ViewController.swift
@@ -12,8 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let apiKey = getKey()
+        print(apiKey)
     }
-
-
+    
+    func getKey() -> String{
+        // get apiKey from Bundle
+        let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+        
+        // check if key is ok
+        guard let key = apiKey, !key.isEmpty else {
+            print("API key does not exist")
+            return ""
+        }
+        
+        return key
+    }
 }
 
