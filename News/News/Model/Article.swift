0b6a2ccb3d79036c6378d8ac44a8f1953de36346
JooYoo <jooyoo@outlook.com>
2022-08-17 14:07:33 +0200

    chore: added model

---
+++ News/News/Model/Article.swift
@@ -0,0 +1,17 @@
+//
+//  Article.swift
+//  News
+//
+//  Created by Yu on 17.08.22.
+//
+
+import Foundation
+
+struct ArticlesList: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let title: String
+    let description: String
+}
