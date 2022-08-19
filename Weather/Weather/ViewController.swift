0eb7ee00fc1901b9078b41c75e469a7ad727750b
JooYoo <jooyoo@outlook.com>
2022-08-19 11:25:39 +0200

    feat: hide key

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(getKey())
     }
 
-
+    func getKey() -> String{
+        // get apiKey from Bundle
+        let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+        
+        // check if key is ok
+        guard let key = apiKey, !key.isEmpty else {
+            print("API key does not exist")
+            return ""
+        }
+        
+        return key
+    }
 }
 
