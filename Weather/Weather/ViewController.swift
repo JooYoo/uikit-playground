c5c9c9d34d8e334f4c7d877d7b6f5d5f2d5ccdfc
JooYoo <jooyoo@outlook.com>
2022-08-20 13:40:56 +0200

    feat: get data from api

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -10,16 +10,28 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-    
+    // Rx
+    let bag = DisposeBag()
+    // Outlets
     @IBOutlet weak var cityNameTextField: UITextField!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        print(getKey())
+        fetchWeather()
+    }
+    
+    func fetchWeather(){
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=ulm&units=metric&appid=\(getKey())")!
+        let resource = Resource<WeatherResult>(url: url)
+        
+        URLRequest.load(resource: resource)
+            .subscribe( onNext: {  weatherResult in
+                    // get weather data
+                    print(weatherResult.main)
+            }).disposed(by: bag)
     }
 
     func getKey() -> String{
