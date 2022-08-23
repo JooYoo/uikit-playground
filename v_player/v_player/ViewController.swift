2fb0c0b28225e5fa726c3a5a18d9b80c589d7189
JooYoo <jooyoo@outlook.com>
2022-08-23 18:20:35 +0200

    feat: play the video when app start

--- v_player/v_player/ViewController.swift
+++ v_player/v_player/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,7 +15,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo()
+    }
+    
+    private func playVideo(){
+        // get the Bundle path
+        guard let path = Bundle.main.path(forResource: "test", ofType: "mp4") else {
+            debugPrint("test.mp4 not found")
+            return
+        }
+        
+        // create player + Controller => combine them
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        
+        // show the player
+        present(playerController, animated: true)
+        
+        // play video when screen load
+        player.play()
+    }
 }
-
