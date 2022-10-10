2e8490d62845817e77ba8b09fb0b2bbc10452c2f
JooYoo <jooyoo@outlook.com>
2022-10-10 20:32:10 +0200

    render data to ui

--- combine-weather/combine-weather/ViewController.swift
+++ combine-weather/combine-weather/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var temperature: UILabel!
+    
+    // get dependency of Webservice
+    private var webservice = Webservice()
+    // Combine: an AnyCancellable instance automatically calls cancel() when deinitialized
+    private var cancelable: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let test = Webservice().fetchWeather(city: "test")
-        print(test)
+        cancelable = webservice
+            .fetchWeather(city: "Ulm")
+            .catch{_ in Just(WeatherInfo.placeholder)}
+            .map{ data in
+                if let temp = data.temp {
+                    return "\(temp) ℃"
+                } else {
+                    return "🐞"
+                }
+            }
+            .assign(to: \.text, on: self.temperature)
     }
 }
 
